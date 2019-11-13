bec8d03079c4f38d6f4f0192ee09fedb9bc4be59
MUSTANGBOSS8055 <anveshakrathore@yahoo.co.in>
2019-11-13 09:39:10 +0530

    comments rip

--- main.py
+++ main.py
@@ -18,155 +18,54 @@ db = sqlite3.connect('pwprotect.db')
 
 
 def insert(sitename, username, password):
-    while True:
-        '''
-        print()
-        sitename = input("Enter the name of the site: ").lower()
-        username = input("Enter your username for the site: ")
-        password = ""
-        ch = input("Do you want to use our generated password?(y/n): ")
-        if ch == "y":
-            password = pwgenerator()
-            print("Your generated password is ", password)
-        elif ch == "n":
-            password = input("Enter your password for the site: ")
-'''
-        enk, key = encrypt(password)
-        entities = (sitename, username, enk, key)
-        cursor = db.cursor()
-        cursor.execute('INSERT INTO INFO(SITENAME,USERNAME,PASSWORD,PWKEY) VALUES (?,?,?,?)', entities)
-        db.commit()
-        '''
-        ch = input("Do you want to make another entry?(y/n): ")
-        if ch == 'n':
-            ch = input("Do you want to return to home screen?(y/n): ")
-            print()
-            if ch == 'y':
-                home()
-                break
 
-            elif ch == 'n':
-                print("Thank you for using Password Database")
-                return
+    enk, key = encrypt(password)
+    entities = (sitename, username, enk, key)
+    cursor = db.cursor()
+    cursor.execute('INSERT INTO INFO(SITENAME,USERNAME,PASSWORD,PWKEY) VALUES (?,?,?,?)', entities)
+    db.commit()
 
-        elif ch == 'y':
-            continue
-
-        else:
-            print("Invalid input")
-            home()
-            break
-'''
-        break
     return
 
 
 def search(site):
-    while True:
-        # site = input('Enter the name of the site: ').lower()
-        csr = db.cursor()
-        csr.execute('SELECT USERNAME, PASSWORD, PWKEY FROM INFO WHERE SITENAME == ?', (site,))
-        row = csr.fetchone()
-        if row is not None:
-            username = row[0]
-            enk = row[1]
-            key = row[2]
-        else:
-            # print("Data not found")
-            # home()
-            return "not found", "not found"
+    csr = db.cursor()
+    csr.execute('SELECT USERNAME, PASSWORD, PWKEY FROM INFO WHERE SITENAME == ?', (site,))
+    row = csr.fetchone()
+    if row is not None:
+        username = row[0]
+        enk = row[1]
+        key = row[2]
+    else:
+        # print("Data not found")
+        # home()
+        return "not found", "not found"
+    password = decrypt(enk, key)
 
-        password = decrypt(enk, key)
-        '''
-        print()
-        print("Username: ",username)
-        print("Password: ",password)
-
-        ch = input("Do you want to search again?(y/n): ")
-        if ch == 'n':
-            ch = input("Do you want to return to home screen?(y/n): ")
-            print()
-            if ch == 'y':
-                home()
-                break
-
-            elif ch == 'n':
-                print("Thank you for using Password Database")
-                return
-
-        elif ch == 'y':
-            continue
-
-        else:
-            print("Invalid input")
-            home()
-            break
-            '''
-        break
     return username, password
 
 
 def update(site, user, passw):
-    while True:
-        # site = input('Enter the name of the site: ').lower()
-        # ch = int(input("1 = Change Username, 2 = Change Password: "))
-        csr = db.cursor()
-        if passw is None:
-            # user = input("Enter new username: ")
-            csr.execute('UPDATE INFO SET USERNAME = ? WHERE SITENAME = ?', (user, site))
-            db.commit()
-            # print("Your new username is ", user)
+    csr = db.cursor()
+    if passw is None:
+        # user = input("Enter new username: ")
+        csr.execute('UPDATE INFO SET USERNAME = ? WHERE SITENAME = ?', (user, site))
+        db.commit()
 
-        elif user is None:
-            # passw = ""
-            # c = input("Do you want to use our generated password?(y/n): ")
-            '''
-            if c == "y":
-                passw = pwgenerator()
-            elif c == "n":
-                passw = input("Enter new password: ")
-            '''
-            enk, key = encrypt(passw)
+    elif user is None:
 
-            csr.execute('UPDATE INFO SET PASSWORD = ? WHERE SITENAME = ?', (enk, site))
-            csr.execute('UPDATE INFO SET PWKEY = ? WHERE SITENAME = ?', (key, site))
-            db.commit()
-            # print("Your new password is ", passw)
+        enk, key = encrypt(passw)
 
-        else:
-            enk, key = encrypt(passw)
-            csr.execute('UPDATE INFO SET USERNAME = ? WHERE SITENAME = ?', (user, site))
-            csr.execute('UPDATE INFO SET PASSWORD = ? WHERE SITENAME = ?', (enk, site))
-            csr.execute('UPDATE INFO SET PWKEY = ? WHERE SITENAME = ?', (key, site))
-            db.commit()
-        '''
-        else:
-            print("Invalid input")
-            home()
-            break
+        csr.execute('UPDATE INFO SET PASSWORD = ? WHERE SITENAME = ?', (enk, site))
+        csr.execute('UPDATE INFO SET PWKEY = ? WHERE SITENAME = ?', (key, site))
+        db.commit()
 
-        ch = input("Do you want to update again?(y/n): ")
-
-        if ch == 'n':
-            ch = input("Do you want to return to home screen?(y/n): ")
-            print()
-            if ch == 'y':
-                home()
-                break
-
-            elif ch == 'n':
-                print("Thank you for using Password Database")
-                return
-
-        elif ch == 'y':
-            continue
-
-        else:
-            print("Invalid input")
-            home()
-            break
-        '''
-        break
+    else:
+        enk, key = encrypt(passw)
+        csr.execute('UPDATE INFO SET USERNAME = ? WHERE SITENAME = ?', (user, site))
+        csr.execute('UPDATE INFO SET PASSWORD = ? WHERE SITENAME = ?', (enk, site))
+        csr.execute('UPDATE INFO SET PWKEY = ? WHERE SITENAME = ?', (key, site))
+        db.commit()
 
 
 def pwgenerator():
